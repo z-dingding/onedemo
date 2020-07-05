596579b4d6d4f72fc8bb29dc542313754cd2c023
bob-dingding <15910513547@163.com>
2020-07-05 22:08:45 +0800

    ES6基本完成

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// 这个文件是一个 ES6语法 的js文件，稍后，我们尝试把这个 ES6 语法的 js 文件转化为 ES5 的 js 文件。
+// PS：我们在写代码时，能用单引号尽量用单引号，而不是双引号，前者在压缩之后，程序执行会更快。
+
+
+
+let a = 'smyhvae';
+const b = 'qianguyihao';
+
+console.log(a);
+console.log(b);
+
