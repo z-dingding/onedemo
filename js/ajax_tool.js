c657a7d81cc297096f9e6574eac15cafb9facc07
bob-dingding <15910513547@163.com>
2020-07-10 18:07:38 +0800

    ajax基本完成

---
+++ js/ajax_tool.js
@@ -0,0 +1,64 @@
+// 方法：将 get 跟post 封装到一起
+/*
+	参数1:url
+	参数2:数据
+	参数3:请求的方法
+	参数4:数据成功获取以后，调用的方法
+*/
+function ajax_tool(url,data,method,success) {
+	// 异步对象
+	var ajax = new XMLHttpRequest();
+
+	// get 跟post  需要分别写不同的代码
+	if (method=='get') {
+		// get请求
+		if (data) {
+			// 如果有值
+			url+='?';
+			url+=data;
+		}else{
+
+		}
+		// 设置 方法 以及 url
+		ajax.open(method,url);
+
+		// send即可
+		ajax.send();
+	}else{
+		// post请求
+		// post请求 url 是不需要改变
+		ajax.open(method,url);
+
+		// 需要设置请求报文
+		ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+		// 判断data send发送数据
+		if (data) {
+			// 如果有值 从send发送
+			ajax.send(data);
+		}else{
+			// 木有值 直接发送即可
+			ajax.send();
+		}
+	}
+
+	// 注册事件
+	ajax.onreadystatechange = function () {
+		// 在事件中 获取数据 并修改界面显示
+		if (ajax.readyState==4&&ajax.status==200) {
+			// console.log(ajax.responseText);
+
+			// 将 数据 让 外面可以使用
+			// return ajax.responseText;
+
+			// 当 onreadystatechange 调用时 说明 数据回来了
+			// ajax.responseText;
+
+			// 数据成功获取以后，执行方法success()。
+			//我们把获取的数据作为 success()的参数，意思是：
+			//success方法是外面的，数据是里面给的。那数据就变相地传递到了外面去【重要】
+			success(ajax.responseText);
+		}
+	}
+
+}
